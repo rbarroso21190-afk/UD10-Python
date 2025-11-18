6ebff1d40ec8ba4168455a1064d17aa11483c8a3
rbarroso21190-afk <rbarroso21190@iesjoanramis.org>
2025-11-18 12:56:59 +0000

    Ejercicio3.19

---
+++ Ejercicio3.19.py
@@ -0,0 +1,24 @@
+"""Definir una funció que donada una cadena, avaluï quantes lletres majúscules hi ha.
+Prova-la amb diferents exemples."""
+
+cadena = input("Introduce una cadena de texto:")
+
+def contar_mayusculas(cadena):
+    contador = 0
+    for caracter in cadena:
+        if caracter.isupper():
+            contador += 1
+    return contador
+
+print("El número de letras mayúsculas en la cadena es:", contar_mayusculas(cadena))
+
+cadena = input("Introduce una cadena de texto:")
+
+def count_mayus(cadena):
+    count = 0
+    for char in cadena:
+        if char.isupper():
+            count += 1
+    return count
+
+print("El número de letras mayúsculas en la cadena es:", count_mayus(cadena))
