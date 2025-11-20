35cfd780827e3b0f1a680b25c61c3c0db2f25088
rbarroso21190-afk <rbarroso21190@iesjoanramis.org>
2025-11-20 19:14:50 +0000

    Ejercicio3.33

---
+++ Ejercicio3.33.py
@@ -0,0 +1,7 @@
+"Escriure un programa que imprimeixi 5 vegades la sèrie de números de l’1 al 15."
+
+for a in range(5):
+    for b in range(1, 16):
+        print(b, end=' ') #Para que esten en la misma fila separados por espacios
+    print("\n")
+
