0e8ebe1c276e5e8b720dd1573e8373c46b953d8d
rbarroso21190-afk <rbarroso21190@iesjoanramis.org>
2025-11-26 12:42:10 +0000

    Ejercicio3.47

---
+++ Ejercicio3.47.py
@@ -0,0 +1,13 @@
+"""Escriure un programa que, utilitzant, la sentència for mostri el següent:
+*
+**
+***
+**
+*
+"""
+
+for i in range(1, 4):
+    print("*" * i)
+
+for i in range(2, 0, -1):
+    print("*" * i)
