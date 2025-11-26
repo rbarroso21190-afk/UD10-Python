27aaa0b8b4842257c25e0ea6c9e54ff1b9b42d03
rbarroso21190-afk <rbarroso21190@iesjoanramis.org>
2025-11-26 12:22:06 +0000

    Ejercicio3.45

---
+++ Ejercicio3.45.py
@@ -0,0 +1,8 @@
+"""Escriure un programa que ens mostri tots els números parells fins arribar a 100. "
+Ex: 2, 4, 6 ...., 98, 100. Prova-la. Fer el mateix amb els números senars."""
+
+a = 0
+
+while a <= 100:
+    print(a, end=" ")
+    a += 2
