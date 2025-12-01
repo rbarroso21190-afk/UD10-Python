d3e65361c7cf5321c73ad39c0c5148a21dddfc62
rbarroso21190-afk <rbarroso21190@iesjoanramis.org>
2025-12-01 07:59:31 +0000

    Ejercicio3.51

---
+++ Ejercicio3.51.py
@@ -0,0 +1,11 @@
+"""Crear una funció que compti la longitud de cada paraula d’una cadena de caràcters que li passem. Utilitzar map. 
+Ex: def lenp(frase): -- retorni una llista amb la longitud de cada paraula de la frase."""
+
+def lenp(frase):
+    palabras = frase.split()                 
+    longitudes = list(map(len, palabras))   
+    return longitudes
+
+frase = input("Introduce una frase:")
+resultado = lenp(frase)
+print(resultado)
