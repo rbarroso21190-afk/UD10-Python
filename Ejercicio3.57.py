8cf65eb08f4f179b1f5e18db9729848664b960bf
rbarroso21190-afk <rbarroso21190@iesjoanramis.org>
2025-12-02 08:34:47 +0000

    Ejercicio3.57

---
+++ Ejercicio3.57.py
@@ -0,0 +1,7 @@
+"""Crear una llista amb les lletres d’una paraula qualsevol. 
+Utilitzar list comprehensions. Ex: “institut”, retorni [‘i’,’n’,’s’,’t’,’i’,’t’,’u’,’t’]."""
+
+palabra = "electroencefalografista"
+
+lista = [palabra for palabra in palabra]
+print(lista)
