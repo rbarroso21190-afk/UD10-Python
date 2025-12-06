7852bccc475e96a7fd2da738c1949014e0d3bef2
rbarroso21190-afk <rbarroso21190@iesjoanramis.org>
2025-12-06 13:45:12 +0000

    Ejercicio3.67

---
+++ Ejercicio3.67.py
@@ -0,0 +1,8 @@
+"""Realitzar un projecte en python seguint les següents indicacions:
+El programa principal ha de permetre seleccionar una de les 6 aplicacions que haureu de programar:
+-La primera funció a desenvolupar ha de permetre treballar amb llistes, números aleatoris.
+-La segona funció a desenvolupar ha de fer feina amb fitxers i ha de permetre guardar-hi informació, com per exemple: una agenda, una llista de la compra, etc. Ha de permetre recuperar la llista, actualitzar-la, esborrar-la, etc.
+-La tercera funció a desenvolupar és un joc com els que hem fet a classe (Mastermind, els dels talaiòtics i gegants, ...)
+-La quarta funció a desenvolupar és una aplicació que treballi amb objectes, classe, herència, polimorfisme, etc.
+-La quinta funció a desenvolupar ha de ser una aplicació que utilitza una llibreria per a treballar amb big data, scrapping, etc.
+-La sexta funció a desenvolupar ha de ser una aplicació que permeti muntar un servei web."""
